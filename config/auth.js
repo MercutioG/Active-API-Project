1ec1803634c59a23cfba25b35d323a1579e1230c
MercutioG <110576904+MercutioG@users.noreply.github.com>
2023-10-27 13:04:04 -0700

    Created config files
    - Created auth and passport files in the config folder

--- config/auth.js
+++ config/auth.js
@@ -1,1 +1,16 @@
-// Placeholder
+const ensureAuthenticated = (req, res, next) => {
+  if(req.isAuthenticated()) {
+    next()
+  } else {
+    req.flash('error_msg', 'Please login to view this resource')
+    res.redirect('/users/login')
+  }
+}
+
+
+
+
+module.exports = {
+  ensureAuthenticated
+}
+// Verifies if the user is logged in, it will return true if they are, otherwise it will send them to the login page
--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,49 @@
-// placeholder
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+// Passport is used to verify usernames and passwords
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy({username: 'email', passwordField: 'password', passReqToCallback: false, session: true}, (email, password, done) => {
+      console.log('Local Strat Works')
+      // match user
+      User.findOne({email: email})
+      .then((user) => {
+        if(!user) {
+          return done(null, false, {message: 'that email is not registered'})
+        }
+        //match pass
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if(err) throw err;
+
+          if(isMatch) {
+            console.log(email + ' ' + password)
+            return done(null, user);
+          } else {
+            return done(null, false, {message: 'Pass Incorrect'})
+          }
+        })
+      })
+      .catch((err) => console.log(err))
+    })
+  )
+
+
+
+
+// Serializing saves the user id to the session, and the deserialization grabs that id (function(ID, done){})
+
+  // These are to handle login sessions
+  passport.serializeUser(function(user, done) {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id).then((err, user) => {
+      console.log(err)
+      if(err) return done(err)
+      done(user, err)
+    }).catch( (err => {console.log(err)}))
+  })
+}
